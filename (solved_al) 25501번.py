8ca4091d759506930fba16a4b2eff96e08c293db
FutureMaker0 <120623320+FutureMaker0@users.noreply.github.com>
2023-05-09 11:36:41 +0900

    recursion

---
+++ (solved_al) 25501번.py
@@ -0,0 +1,43 @@
+'''
+팰린드롬인지 판별하는 불린 출력, recursion 함수 호출횟수
+
+★★★ 문자열 LIST 입력 받기 ★★★
+
+'''
+
+def recursion(s, l, r):
+    if l >= r:
+        return 1, (l+1)
+    elif s[l] != s[r]: 
+        return 0, (l+1)
+    else: 
+        return recursion(s, l+1, r-1)
+
+def isPalindrome(s):
+    return recursion(s, 0, len(s)-1)
+
+T = int(input())
+arr = [[] for _ in range(T)]
+# count = [[0] for _ in range(T)] 
+
+for i in range(T):
+    arr[i] = input()
+    # str = arr[i]
+    # print(len(str))
+    # print(arr[i])
+    
+for i in range(T):
+    print(*isPalindrome(arr[i])) #, recursion 횟수)
+    # print(*count[i])
+
+
+
+
+
+# print('ABBA:', isPalindrome('ABBA'))
+# print('ABC:', isPalindrome('ABC'))
+
+
+
+
+
