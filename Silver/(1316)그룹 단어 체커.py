a3401bf4d0c2d77b202dd31288c091fcee75f839
JaeHyeonKim <120623320+FutureMaker0@users.noreply.github.com>
2023-11-23 12:25:11 +0900

    Update (1316)그룹 단어 체커.py

--- Silver/(1316)그룹 단어 체커.py
+++ Silver/(1316)그룹 단어 체커.py
@@ -4,11 +4,6 @@ answer = T
 for i in range(T):
     N = str(input())
 
-    '''
-    문자열 그룹단어 여부 판별 알고리즘
-    - 연속성이 끊겼을 때 다시 해당 문자가 안나오면 cnt+1
-    '''
-
     for j in range(len(N)-1):
         if N[j] == N[j+1]:
             pass
